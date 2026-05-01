16ec2e0468fce96c9be497591d269a682210472a
Akmal Keisin <akmalkeisin@gmail.com>
2026-05-01 20:54:53 +0700

    UPDATE - setup table

--- resources/js/pages/admin/category/Index.vue
+++ resources/js/pages/admin/category/Index.vue
@@ -1,48 +1,109 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
-import { onMounted, ref } from 'vue';
-import article from '@/routes/article';
-import type { Category } from '@/types/model/category';
-import { columns } from './component/colummn';
-import DataTable from './component/data-table.vue';
+import Table from '@/components/ui/table/Table.vue';
+import TableBody from '@/components/ui/table/TableBody.vue';
+import TableCaption from '@/components/ui/table/TableCaption.vue';
+import TableCell from '@/components/ui/table/TableCell.vue';
+import TableFooter from '@/components/ui/table/TableFooter.vue';
+import TableHead from '@/components/ui/table/TableHead.vue';
+import TableHeader from '@/components/ui/table/TableHeader.vue';
+import TableRow from '@/components/ui/table/TableRow.vue';
+import category from '@/routes/category';
 
 defineOptions({
     layout: {
         breadcrumbs: [
             {
-                title: 'Article',
-                href: article.index(),
+                title: 'Category',
+                href: category.index(),
             },
         ],
     },
 });
 
-const data = ref<Category[]>([]);
-async function getData(): Promise<Category[]> {
-    return [
-        {
-            id: 1,
-            name: 'Politic',
-            article_count: 0,
-            created_at: '2025-01-02',
-            updated_at: '2025-01-03',
-        },
-    ];
-}
-
-onMounted(async () => {
-    data.value = await getData();
-});
+const invoices = [
+    {
+        invoice: 'INV001',
+        paymentStatus: 'Paid',
+        totalAmount: '$250.00',
+        paymentMethod: 'Credit Card',
+    },
+    {
+        invoice: 'INV002',
+        paymentStatus: 'Pending',
+        totalAmount: '$150.00',
+        paymentMethod: 'PayPal',
+    },
+    {
+        invoice: 'INV003',
+        paymentStatus: 'Unpaid',
+        totalAmount: '$350.00',
+        paymentMethod: 'Bank Transfer',
+    },
+    {
+        invoice: 'INV004',
+        paymentStatus: 'Paid',
+        totalAmount: '$450.00',
+        paymentMethod: 'Credit Card',
+    },
+    {
+        invoice: 'INV005',
+        paymentStatus: 'Paid',
+        totalAmount: '$550.00',
+        paymentMethod: 'PayPal',
+    },
+    {
+        invoice: 'INV006',
+        paymentStatus: 'Pending',
+        totalAmount: '$200.00',
+        paymentMethod: 'Bank Transfer',
+    },
+    {
+        invoice: 'INV007',
+        paymentStatus: 'Unpaid',
+        totalAmount: '$300.00',
+        paymentMethod: 'Credit Card',
+    },
+];
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <div
-        class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
-    >
-        <div class="container mx-auto py-10">
-            <DataTable :columns="columns" :data="data" />
+    <div class="relative flex flex-col overflow-auto p-4 lg:px-6">
+        <div class="overflow-hidden rounded-lg border">
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader class="sticky top-0 z-10 bg-muted">
+                    <TableRow>
+                        <TableHead class="w-25"> Invoice </TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead class="text-right"> Amount </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody class="**:data-[slot=table-cell]:first:w-8">
+                    <TableRow
+                        v-for="invoice in invoices"
+                        :key="invoice.invoice"
+                    >
+                        <TableCell class="font-medium">
+                            {{ invoice.invoice }}
+                        </TableCell>
+                        <TableCell>{{ invoice.paymentStatus }}</TableCell>
+                        <TableCell>{{ invoice.paymentMethod }}</TableCell>
+                        <TableCell class="text-right">
+                            {{ invoice.totalAmount }}
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colspan="3"> Total </TableCell>
+                        <TableCell class="text-right"> $2,500.00 </TableCell>
+                    </TableRow>
+                </TableFooter>
+            </Table>
         </div>
     </div>
 </template>
